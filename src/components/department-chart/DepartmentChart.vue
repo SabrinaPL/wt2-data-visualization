24a5cb88c6220b2fdf9eb85013ec1491f89584cb
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2025-05-12 19:55:29 +0200

    Start of department chart component

---
+++ src/components/department-chart/DepartmentChart.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import * as echarts from 'echarts';
+import { ref, onMounted, watch } from 'vue';
+import { useGenderStatisticsStore } from '../../stores/genderStatisticsStore';
+
+// Reference the bar chart container
+const chartRef = ref<HTMLDivElement | null>(null);
+let chartInstance: echarts.ECharts | null = null;
+
+// Access the store
+const genderStatisticsStore = useGenderStatisticsStore();
+
+// Reactive variables
+const selectedDepartment = ref<string>('Acting'); // Default selected department
+const selectedViewMode = ref<string>('count'); // Default view mode (count or percentage)
+
+const departmentData = ref<{ men: number; women: number; undefined: number }>({
+  men: 0,
+  women: 0,
+  undefined: 0,
+});
+
+const isLoading = ref(true);
+
+
